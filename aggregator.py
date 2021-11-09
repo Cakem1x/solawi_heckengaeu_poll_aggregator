96e275f25308a62fab82268800cf62f8e6f4441d
Cakemix <mholoch@gmail.com>
2021-11-09 20:16:58 +0100

    add: filter assets that are not of interest

--- aggregator.py
+++ aggregator.py
@@ -14,6 +14,7 @@ def cli():
                         help="Kpi url used to retrieve data from.")
     args = parser.parse_args()
 
+    # TODO: rm pickle stuff, only temporary, for quicker dev
     json_response = None
     pickle_path = "json_response.pkl"
     if os.path.isfile(pickle_path):
@@ -23,8 +24,9 @@ def cli():
         json_response = download_assets(args.kpi_url, args.api_token)
         with open(pickle_path, "wb") as pickle_file:
             pickle.dump(json_response, pickle_file)
-    print(len(json_response["results"]))
-    print(json_response["results"][0])
+
+    print("Got {} assets in total".format(len(json_response["results"])))
+    print(list(filter_assets(json_response, "Erntemengen - KW"))[0])
 
 def download_assets(kpi_url: str, api_token: str):
     get_assets_url = "https://{}/api/v2/assets.json".format(kpi_url)
@@ -33,6 +35,16 @@ def download_assets(kpi_url: str, api_token: str):
     response.raise_for_status()
     json_response = response.json()
     return json_response
+
+def filter_assets(json_response, asset_name_infix):
+    for asset in json_response['results']:
+        if not asset['has_deployment']: # only parse data from polls that are complete
+            print("Filtered asset {} because it never was deployed.".format(asset['name']))
+            continue
+        if asset_name_infix not in asset['name']:
+            print("Filtered asset {} due to name mismatch (does not contain '{}').".format(asset['name'], asset_name_infix))
+            continue
+        yield asset
 
 if __name__ == "__main__":
     cli()
